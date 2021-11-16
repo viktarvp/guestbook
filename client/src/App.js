e81628a8439ba12b3800a778872b6fbdc5b72065
viktar <vpiatrou@i-deasoft.com>
2021-11-16 22:26:36 +0300

    Code polishing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-import CommentsList from './components/CommentsList';
 import InputForm from './components/InputForm';
 import { Container, Row, Col } from 'react-bootstrap';
-import './App.css';
 
 function App() {
   return (
@@ -12,7 +10,6 @@ function App() {
           <Col xs={6}>
             <h1 class="text-center">Guest book</h1>
             <InputForm />
-            {/* <CommentsList /> */}
           </Col>
           <Col></Col>
         </Row>
