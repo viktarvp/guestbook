9700ff465415fab642797e6344ebdd82692fbd35
viktar <vpiatrou@i-deasoft.com>
2021-11-17 01:35:57 +0300

    Code polishing

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.use(express.json({ extended: true }));
 app.use('/api', require('./routes/comment.routes'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, '/client/index.html'))
   );
 }
 
